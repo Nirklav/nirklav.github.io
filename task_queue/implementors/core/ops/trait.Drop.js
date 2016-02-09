0ea2ad1340c18ddfb03e4264c363b901f9924f09
Admin <mrAnton911@gmail.com>
2016-02-09 22:18:45 +0200

    task_queue doc update

---
+++ task_queue/implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['task_queue'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='task_queue/struct.TaskQueue.html' title='task_queue::TaskQueue'>TaskQueue</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
