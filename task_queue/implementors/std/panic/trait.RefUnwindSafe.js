29333a0e2ed28635a96ff971b42be2cd452e5be7
Nirklav <mrAnton911@gmail.com>
2016-07-09 23:54:03 +0300

    task_queue doc update

---
+++ task_queue/implementors/std/panic/trait.RefUnwindSafe.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['task_queue'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html' title='std::panic::RefUnwindSafe'>RefUnwindSafe</a> for <a class='struct' href='task_queue/struct.Task.html' title='task_queue::Task'>Task</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
