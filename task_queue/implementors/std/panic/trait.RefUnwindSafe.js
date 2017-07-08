669c8fc17a5b4de6f916c038a83a1058d57b64da
Nirklav <mrAnton911@gmail.com>
2017-07-08 14:32:29 +0300

    task_queue doc update

--- task_queue/implementors/std/panic/trait.RefUnwindSafe.js
+++ task_queue/implementors/std/panic/trait.RefUnwindSafe.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['task_queue'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html' title='std::panic::RefUnwindSafe'>RefUnwindSafe</a> for <a class='struct' href='task_queue/struct.Task.html' title='task_queue::Task'>Task</a>",];
+implementors["task_queue"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.RefUnwindSafe.html\" title=\"trait std::panic::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"task_queue/struct.Task.html\" title=\"struct task_queue::Task\">Task</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
